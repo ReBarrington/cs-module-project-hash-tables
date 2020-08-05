04c88f7f3027e25fc8ce7cb1f3882d8429f4ce6e
Reagan Barrington <reagan@barringtonfamily.net>
2020-08-05 15:56:48 -0400

    expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -4,17 +4,18 @@ cache = {}
 
 def expensive_seq(x, y, z):
     # Your code here
-
-    # base case
     if x <= 0:
         return y + z
 
-    if x not in cache:
-        # store in cache
-        cache[x] = {'1': expensive_seq(x-1,y+1,z), '2': expensive_seq(x-2,y+2,z*2), '3': expensive_seq(x-3,y+3,z*3)}
+    elif (x, y, z) in cache:
+        v = cache.get((x, y, z))
+    
+    else:
+        v = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+        cache.update({ (x, y, z): v })
 
-    print(cache[x], ' is cache of x')
-    return cache[x]['1'] + cache[x]['2']+ cache[x]['3']
+    return v
+
 
 if __name__ == "__main__":
     for i in range(10):
